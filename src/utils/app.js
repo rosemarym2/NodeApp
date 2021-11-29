09096e9ae01e8c2fdff3ad8a28ade6cd416f9ce5
rosemarym2 <rosemarymay91@gmail.com>
2021-11-29 12:13:16 +0000

    commit

--- src/utils/app.js
+++ src/utils/app.js
@@ -6,9 +6,26 @@ const app = (args) => {
           const movie = new Movie (args[3], args[4]);
           movie.add();
           console.log(movieArr);
+          break;    
+      case "add multi":
+          const movie1 = new Movie (args[3], (args[4]));
+          const movie2 = new Movie (args[5], (args[6]));
+          const movie3 = new Movie (args[7], (args[8]));
+          const movie4 = new Movie (args[9], (args[10]));
+          const movie5 = new Movie (args[11], (args[12]));
+          const movie6 = new Movie (args[13], (args[14]));
+          movie1.add();
+          movie2.add();
+          movie3.add();
+          movie4.add();
+          movie5.add();
+          movie6.add();
+          console.log(movieArr);
           break;
-        case "add multi":
-            const movie1 = new Movie (args[3], (args[4]));
-            const movie2 = new Movie (args[5], (args[6]));
+     default:
+          console.log("Incorrect Command");
+          break;
   }  
-}
+};
+
+app(process.argv);
