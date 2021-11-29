5f8feb6819e5874f3862645187af26ed85d42ea8
rosemarym2 <rosemarymay91@gmail.com>
2021-11-29 14:10:46 +0000

    sixth commit

--- src/utils/app.js
+++ src/utils/app.js
@@ -3,7 +3,7 @@ const {Movie, movieArr} = require("./utils");
 const app = (args) => {
   switch (args[2]){
       case "add":
-          const movie = new Movie (args[3], args[4]);
+          const movie = new Movie (args[3], args[4], args[5]);
           movie.add();
           console.log(movieArr);
           break;    
